09380120889fb4cd98fca10f2b3973f7f7c27c50
16plus1 <57999800+16plus1@users.noreply.github.com>
2022-06-07 14:10:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// const { resolveSoa } = require('dns')
-// const express = require('express')
-// const path = require('path')
-// const { app } = require('process')
+const { resolveSoa } = require('dns')
+const express = require('express')
+const path = require('path')
+const { app } = require('process')
 
-// const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 
-// const app = express()
-// app.use(express.static(__dirname))
-// app.use(express.static(path.resolve(__dirname, "build")))
+const app = express()
+app.use(express.static(__dirname))
+app.use(express.static(path.resolve(__dirname, "build")))
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
-// app.listen(PORT)
+app.listen(PORT)
